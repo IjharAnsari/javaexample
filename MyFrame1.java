cd5c56846794f0afa0df5ea1ac9bde217c108281
Ijhar ansari <35225003+IjharAnsari@users.noreply.github.com>
2018-01-09 16:27:25 +0000

    Create MyFrame1.java
    This file is a java file which contains examples of Frame in awt using inheritance.

---
+++ MyFrame1.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class MyFrame extends Frame{
+	MyFrame(String s){
+		super(s);
+		setSize(400, 400);
+		setVisible(true);	
+	}
+	public static void main(String... args){
+		new MyFrame("Hello iJ");
+	}
+}
