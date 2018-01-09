32d343a28cc7be59bf4a7bb53a69fc01e779c972
Ijhar ansari <35225003+IjharAnsari@users.noreply.github.com>
2018-01-09 16:22:59 +0000

    Update MyFrame.java

--- MyFrame.java
+++ MyFrame.java
@@ -1,1 +1,14 @@
+import java.awt.*;
 
+public class MyFrame{
+	Frame f;
+	MyFrame(String s){
+		f=new Frame(s);
+		f.setSize(800,200);
+		f.setVisible(true);
+	}
+	public static void main(String... args){
+	new MyFrame("Ijhar ansari");
+	new MyFrame("iJ");
+}
+}
